fd542a6f76443b40d0e62393b699e82d0527c3d8
Lee Long Hui <longhui@malaysiakini.com>
2021-04-27 18:34:26 +0800

    first commit

---
+++ scrapper.js
@@ -0,0 +1,40 @@
+const cheerio = require('cheerio')
+const axios = require('axios')
+const fs = require('fs')
+const writeStream = fs.createWriteStream('govmy.csv')
+
+writeStream.write(`name,url\n`)
+
+const PAGE =
+  'https://en.wikipedia.org/wiki/List_of_federal_agencies_in_Malaysia'
+
+const hasLink = link => {
+  if (link) {
+    return false
+  }
+  return true
+}
+
+const scrapePage = async () => {
+  try {
+    const res = await axios.get(PAGE)
+    const $ = cheerio.load(res.data)
+
+    $('.mw-parser-output > ul > li').each((i, el) => {
+      const name = $(el)
+        .text()
+        .replace(/,/, '')
+
+      const link = $(el)
+        .find('a')
+        .attr('href')
+      // .replace(/,/, '')
+
+      writeStream.write(`${name},${!!link ? link : null}\n`)
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+scrapePage()
